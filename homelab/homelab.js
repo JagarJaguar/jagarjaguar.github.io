d689c62341feff5c99929e4c924ac43e7034f041
Ashton <thejagar22@gmail.com>
2026-09-23 21:19:42 -0600

    Removed AC Mesh from networking, no longer used.

--- homelab/homelab.js
+++ homelab/homelab.js
@@ -19,8 +19,7 @@ const HOMELAB = {
     { name: "Cloudflare DNS", icon: "cloudflare", note: "proxied" },
     { name: "Ubiquiti Dream Machine Pro", icon: "ubiquiti", note: "router + firewall" },
     { name: "NETGEAR GS724TPv2", icon: "netgear", note: "switch" },
-    { name: "Access Points 1 & 2", icon: "ubiquiti-unifi", note: "2x U7 Pro XG" },
-    { name: "AC Mesh", icon: "ubiquiti-unifi", note: "Ubiquiti AC Mesh" }
+    { name: "Access Points 1 & 2", icon: "ubiquiti-unifi", note: "2x U7 Pro XG" }
   ],
 
   // Things that are true about all nodes.
